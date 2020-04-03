8ab4c30317d0cc017b7b1a4e8afe439b7bc15b62
animeshgoyal9 <animeshgoyal9@gmail.com>
2020-04-03 16:06:46 -0500

    ports

--- rcrs-server-master/boot/kill_rcrs.sh
+++ rcrs-server-master/boot/kill_rcrs.sh
@@ -1,6 +1,23 @@
 #! /bin/bash
 
-pkill -9 xterm 
+now=$(date +"%T")
+echo "Current time : $now"
+
+pid=$(pidof xterm $!)
+echo "Current pid : $pid"
+
+totalpid="$pid-$now"
+echo "Total pid : $totalpid"
+
+pids=( $pid )
+echo "**************************************"
+echo "**************Killing Process*********"
+echo "**************************************"
+echo "First pid: ${pids[0]}"
+echo "Second pid: ${pids[-1]}"
+
+kill -9 ${pids[-1]}
+#pkill -9 xterm 
 
 
 # echo "testing.py Executed at `date` with the PID of: $!" >> my_PIDs.log
