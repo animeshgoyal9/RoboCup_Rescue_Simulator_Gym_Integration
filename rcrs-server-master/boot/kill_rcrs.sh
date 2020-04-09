9f05ae0d75fa600487641702abf9fe0c99c1d7d1
animeshgoyal9 <animeshgoyal9@gmail.com>
2020-04-09 00:36:16 -0500

    kill file

--- rcrs-server-master/boot/kill_rcrs.sh
+++ rcrs-server-master/boot/kill_rcrs.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 
 now=$(date +"%T")
 echo "Current time : $now"
@@ -9,22 +9,14 @@ echo "Current pid : $pid"
 # totalpid="$pid-$now"
 # echo "Total pid : $totalpid"
 
-pids=( $pid )
-# echo "**************************************"
-# echo "**************Killing Process*********"
-# echo "**************************************"
-# echo "First pid: ${pids[0]}"
-# echo "Second pid: ${pids[-1]}"
+PidArray=($pid)
 
-kill -9 ${pids[-1]}
+# for ((i=0; i<${#PidArray[@]}; ++i)); do
+#     echo "pid $i:${PidArray[$i]}"
+# done
 
-rm -rf logs-*
-rm -rf *_tensorboard
-rm -rf __pycache__
+echo "Last Pid: ${PidArray[-1]}"
+
+kill -9 ${PidArray[-1]}
+
 #pkill -9 xterm 
-
-
-# echo "testing.py Executed at `date` with the PID of: $!" >> my_PIDs.log
-# exit 0"
-
-# ps axf | grep "xterm" & $! | grep -v grep | awk '{print "kill -9 " $1}'
--- rcrs-server-master/boot/testing_condor.sh
+++ rcrs-server-master/boot/testing_condor.sh
@@ -14,4 +14,4 @@
 # echo $XVFB_PID
 # ps -efw | grep Xvfb
 
-xvfb-run -a python testing.py $1 $2 $3 $4 $5 $6 $7
+xvfb-run -a python testing.py $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
